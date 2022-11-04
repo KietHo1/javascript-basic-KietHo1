cb8a879a14f764f564966cd650d9bcfe30e3c8f1
KietHo1 <kiethostartup1999@gmail.com>
2022-11-04 10:21:32 +0700

    source chapter 2, video 8

--- video7/video7.js
+++
@@ -1,8 +0,0 @@
-console.log(`"Hello world from HTML`.toUpperCase());
-let a = 5;
-let b = 10;
-console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
-console.log("Fifteen is " + a + b + " and not " + (2 * a + b) + ".");
-
-console.log(`Hoi dan it check strings: a = ${a} b= ${b} a+b = ${a + b}`);
-console.log(`/hoidanit/${a}`);

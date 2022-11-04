94c413f3629ee233358e3c367a3b827103c4f265
KietHo1 <kiethostartup1999@gmail.com>
2022-11-04 15:25:05 +0700

    ADD

--- video11/video11.js
+++ video11/video11.js
@@ -3,7 +3,7 @@ console.log(`"Hello world from HTML`);
 let arr = ["Mu", "ManCity", "Liver", "Chelsea"];
 
 let i = 0;
-
+//let chỉ chạy code tring hàm, var chạy cả code bên ngoài hàm
 for (let i = 0; i < arr.length; i++) {
   console.log("check variable i = ", arr[i]);
 }

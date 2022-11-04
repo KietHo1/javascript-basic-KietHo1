077cb0a209cac0066aa4ba377ca1b77f3de1509b
KietHo1 <kiethostartup1999@gmail.com>
2022-11-04 14:40:14 +0700

    ADD

--- video10/video10.js
+++ video10/video10.js
@@ -1,18 +1,19 @@
 console.log(`"Hello world from HTML`);
-let a = {
-  name: "Eric",
-  age: 25,
-}; //key:value
 
-console.log("check data object: ", a);
-console.log("my name's : ", a.name);
+let a = 5; // x <- y
+let b = "5";
 
-//arr = [item1, item2, item3]
-let b = ["MU", "Chelsea", "Liver"];
-console.log("typof b: ", typeof b, b);
-console.log("first item: ", b[0], " second item: ", b[1]);
-let family = [
-  { name: "Eric", age: 25 },
-  { name: "Monica", age: 24 },
-];
-console.log("my family: ", family);
+console.log(" compare a == b: ", "a = ", a, " b = ", a, " result: ", a == b);
+console.log(" compare a === b: ", "a = ", a, " b = ", a, " result: ", a === b);
+// === type, value convention
+
+// Empty, null, undefined
+//Empty và null cùng có nghĩa là rỗng
+//Khác nhau:
+//Empty đã lưu trong bộ nhớ
+//Null chưa lưu trong bộ nhớ
+let c = null; //null
+console.log("check value: ", c);
+let d; //undefined
+d = 5;
+d = []; //Empty

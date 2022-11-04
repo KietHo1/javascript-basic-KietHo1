a2b92b1b6be5d9c8c44a6ccb71d4112cead27b50
KietHo1 <kiethostartup1999@gmail.com>
2022-11-04 15:33:15 +0700

    ADD

--- video12/video12.js
+++ video12/video12.js
@@ -1,12 +1,1 @@
 console.log(`"Hello world from HTML`);
-
-let arr = ["Mu", "ManCity", "Liver", "Chelsea"];
-
-let i = 0;
-//let chỉ chạy code trong hàm, var chạy cả code bên ngoài hàm
-for (let i = 0; i < arr.length; i++) {
-  console.log("check variable i = ", arr[i]);
-}
-
-//
-console.log("check value i: ", i);

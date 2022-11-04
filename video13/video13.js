d427c56f8db7b6baa82203288c678fd77d183ca8
KietHo1 <kiethostartup1999@gmail.com>
2022-11-04 16:15:40 +0700

    ADD

--- video13/video13.js
+++ video13/video13.js
@@ -1,1 +1,24 @@
 console.log(`"Hello world from HTML`);
+
+let arrTop4 = ["Live", "Chelsea", "Mu", "Man city"];
+
+let i = 0;
+//Dùng continue
+while (i < arrTop4.length) {
+  i++;
+  if (arrTop4[i] === "Chelsea") {
+    console.log("Found it: ", arrTop4[i]);
+    continue;
+  }
+
+  console.log(">>> check i: ", i);
+}
+//Dùng break
+while (i < arrTop4.length) {
+  if (arrTop4[i] === "Chelsea") {
+    console.log("Found it: ", arrTop4[i]);
+    break;
+  }
+  i++;
+  console.log(">>> check i: ", i);
+}

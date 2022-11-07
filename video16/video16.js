655a6d26d31177d6602054b19b4b56b5b614bf24
KietHo1 <kiethostartup1999@gmail.com>
2022-11-07 09:24:07 +0700

    chapter 4 video 1

--- video16/video16.js
+++ video16/video16.js
@@ -1,11 +1,10 @@
 console.log(`"Hello world from HTML`);
 
-//global
-//block
-
-// console.log('value x: ', x)
-// let x = 5;
-
-for (let i = 0; i < 10; i++) {
-  console.log(">>> check value i: ", i);
+//input paramater
+function sum(a, b) {
+  return a + b;
 }
+
+let c = sum(10, 20);
+
+console.log("sum c ", c);
